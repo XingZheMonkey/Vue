ebfa97466643b638c9c0e4f80b593335d7bef4d2
XingZheMonkey <50906069+XingZheMonkey@users.noreply.github.com>
2021-06-06 11:38:37 +0800

    Delete vue-pratice directory

--- vue-pratice/src/icons/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import SvgIcon from '../components/Icon/SvgIcon.vue'
-
-Vue.component('svg-icon',SvgIcon)
-
-const req = require.context('./svg',false,/\.svg$/)
-
-req.keys().map(req)
